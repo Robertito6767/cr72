86506b35267dec3f474a2fec02533bf2ef555207
Robertito6767 <robertomontelongo21.12.10@gmail.com>
2026-06-18 15:23:48 -0600

    Update YouTube video link in THE_BEST.py

--- THE_BEST.py
+++ THE_BEST.py
@@ -328,7 +328,7 @@ st.markdown("""
   <h2>🎬 VER LOS MEJORES CLIPS DE CR7</h2>
   <p>Goles imposibles, regates imposibles, momentos que quedaron en la historia del fútbol</p>
   <a class="video-btn"
-     href="https://www.youtube.com/watch?v=1RFA9B5dGxw"
+     href="https://www.youtube.com/watch?v=29mtSn1RdMQ"
      target="_blank"
      rel="noopener noreferrer">
     ▶ &nbsp; VER VIDEO EN YOUTUBE
